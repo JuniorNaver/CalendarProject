39ed0e915ba31421aaf01d6b4d8e2198d016c501
JuniorNaver <withjunhee@gmail.com>
2025-07-16 00:36:17 +0900

    login.js 반응형 스타일showMessage 함수 추가 (오류 시 .fail 성공 시 .success 클래스 붙임(add)), singup.css ui 개선, 오류 수정

--- js/login.js
+++ js/login.js
@@ -10,7 +10,7 @@ loginForm.addEventListener('submit', (e) => {
 
     // 아이디나 비밀번호가 비어있으면 함수를 종료합니다.
     if (!userId || !password) {
-        messageDiv.textContent = '아이디와 비밀번호를 모두 입력해주세요.';
+        showMessage('아이디와 비밀번호를 모두 입력해주세요.', 'error');
         return;
     }
 
@@ -19,7 +19,7 @@ loginForm.addEventListener('submit', (e) => {
     
     //아이디 존재 여부 확인
     if (!userDataStr) { 
-        messageDiv.textContent = '존재하지 않는 아이디입니다.';
+        showMessage('존재하지 않는 아이디입니다.', 'error');
         return; 
     }
   
@@ -28,7 +28,7 @@ loginForm.addEventListener('submit', (e) => {
     // 비밀번호 일치 여부 확인
     if (userData.pwtrim === password) {
         // 로그인 성공
-        messageDiv.textContent = `로그인 성공! 환영합니다, ${userData.username || userId}님.`; // 저장된 이름이 있다면 보여줍니다.
+        showMessage(`로그인 성공! 환영합니다, ${userData.username || userId}님.`, 'success'); // 저장된 이름이 있다면 보여줍니다.
         loginForm.reset(); // 성공 시에만 폼을 초기화합니다.
         
         // 1초 후 메인 페이지로 이동
@@ -38,6 +38,16 @@ loginForm.addEventListener('submit', (e) => {
 
     } else {
         // 로그인 실패
-        messageDiv.textContent = '비밀번호가 틀렸습니다.';
+        showMessage('비밀번호가 틀렸습니다.', 'error');
     }
 });
+
+function showMessage(message, classType) {
+    messageDiv.textContent = message;
+
+    // 이전 클래스 제거
+    messageDiv.classList.remove('success', 'error');
+
+    // 새 클래스 추가(파라미터로 전달)
+    messageDiv.classList.add(classType); // 'success' 또는 'error'
+}
